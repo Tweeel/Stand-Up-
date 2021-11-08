dd7db9117b90a7a01fef3d26cb0a2fc43018f209
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-08 12:01:19 +0100

    Set up the Stand Up! project and views

--- app/src/main/java/com/example/stand_up_/MainActivity.java
+++ app/src/main/java/com/example/stand_up_/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.stand_up_;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Find the ToggleButton by id.
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        alarmToggle.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton compoundButton,
+                                                 boolean isChecked) {
+                        String toastMessage;
+                        if(isChecked){
+                            //Set the toast message for the "on" case.
+                            toastMessage = "Stand Up Alarm On!";
+                        } else {
+                            //Set the toast message for the "off" case.
+                            toastMessage = "Stand Up Alarm Off!";
+                        }
+
+                        //Show a toast to say the alarm is turned on or off.
+                        Toast.makeText(MainActivity.this, toastMessage,Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+
     }
 }
